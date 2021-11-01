22fcf309cbd693e3e260a44d6812d2f69d3a0e3d
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2021-11-01 22:07:55 +0200

    📦 Add a local SharedKernel package

---
+++ SharedKernel/src/SharedKernel/MessagingConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedKernel
+{
+    public static class MessagingConstants
+    {
+        public static class Exchanges
+        {
+            public const string FRONTDESK_VETCLINICPUBLIC_EXCHANGE = "frontdesk-vetclinicpublic";
+        }
+
+        public static class Queues
+        {
+            public const string FDVCP_FRONTDESK_IN = "fdvcp-frontdesk-in";
+            public const string FDVCP_VETCLINICPUBLIC_IN = "fdvcp-vetclinicpublic-in";
+        }
+    }
+}
